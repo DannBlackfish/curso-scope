5fcd2f112887b312814631d7b646d1b79afb5ed5
Daniela Alonzo <monsterjunior@outlook.es>
2021-06-14 12:30:53 -0500

    Debugging

---
+++ src/debug/index.js
@@ -0,0 +1,31 @@
+//palabra reservada debugger que permite pauar el codigo para entender que esta sucedido
+var a = 'Hello';
+
+function hello() {
+    let b = 'Hello World';
+    const c = 'Hello World!'
+    if (true) {
+        let d = 'Hello World!!'
+        debugger
+    }
+};
+
+hello();
+
+//
+const moneyBox = () => {
+    debugger
+    var saveCoins = 0;
+    const countCoins = (coins) => {
+        debugger
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`)
+    }
+    return countCoins;
+};
+
+let myMoneyBox = moneyBox()
+
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(10);
