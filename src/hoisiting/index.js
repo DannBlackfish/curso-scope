40fde2b1633a9219b8ba0969154fe0eaf0cf0cc9
Daniela Alonzo <monsterjunior@outlook.es>
2021-06-14 12:18:10 -0500

    Hoisitng

---
+++ src/hoisiting/index.js
@@ -0,0 +1,14 @@
+//El hoistin eleva las declaraciones
+a = 2;
+var a;
+console.log(a);
+
+//Intentams acceder a la variable a pero esta no esta disponible
+console.log(a)
+var a = 2;
+
+//El motor que compila el codigo en javascript antes de ser interpretado, lo pone primero en la memoria y despues ejecuta la función  
+nameOfDog('Jodie');
+function nameOfDog(name) {
+    console.log(name)
+}
